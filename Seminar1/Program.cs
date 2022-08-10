5c4a699d8d4a9e4c88eeed8cfc35e843d324b83d
Daria <iwahnowa.darja@yandex.ru>
2022-08-10 20:36:22 +0300

    добавили решение второй задачи

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -20,46 +20,54 @@
 //Задача №2. Напишите программу, которая на вход принимает два числа
 //и проверяет, является ли первое число квадратом второго.
 
+Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+    Console.Write("First number is a square of second number");
+else
+    Console.Write("First number is not a square of second number");
 
 //Задача №3. Напишите программу, которая будет выдавать значение
 // дня недели по заданному номеру.
 
-Console.Write("Input number of the day: ");
+//Console.Write("Input number of the day: ");
 
-int day = Convert.ToInt32(Console.ReadLine());
+//int day = Convert.ToInt32(Console.ReadLine());
 
-if (day < 1 || day > 7)
-{
-    Console.Write("Incorrect number");
-}
-else
-{
-    if (day == 1)
-    {
-    Console.Write("Monday");
-    }
-    if (day == 2)
-    {
-    Console.Write("Tuesday");
-    }
-    if (day == 3)
-    {
-    Console.Write("Wednesday");
-    }
-    if (day == 4)
-    {
-    Console.Write("Thursday");
-    }
-    if (day == 5)
-    {
-    Console.Write("Friday");
-    }
-    if (day == 6)
-    {
-    Console.Write("Saturday");
-    }
-    if (day == 7)
-    {
-    Console.Write("Sunday");
-    }
-}
+//if (day < 1 || day > 7)
+//{
+//    Console.Write("Incorrect number");
+//}
+//else
+//{
+//    if (day == 1)
+//    {
+//    Console.Write("Monday");
+//    }
+//    if (day == 2)
+//    {
+//    Console.Write("Tuesday");
+//    }
+//    if (day == 3)
+//    {
+//    Console.Write("Wednesday");
+//    }
+//    if (day == 4)
+//    {
+//    Console.Write("Thursday");
+//    }
+//    if (day == 5)
+//    {
+//    Console.Write("Friday");
+//    }
+//    if (day == 6)
+//    {
+//    Console.Write("Saturday");
+//    }
+//    if (day == 7)
+//    {
+//    Console.Write("Sunday");
+//    }
+//}

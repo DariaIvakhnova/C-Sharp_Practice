9fe0ce453c460616d283fb6570c70dba156ab7a3
Daria <iwahnowa.darja@yandex.ru>
2022-08-13 18:57:06 +0300

    debug task7

--- HomeWork1/Task7_extra/Program.cs
+++ HomeWork1/Task7_extra/Program.cs
@@ -4,11 +4,11 @@
 Console.Clear();
 Console.Write("Введите трехзначное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-if (N < 0) N = -N;
-if (99 < N < 1000)
+if (N < 0) N = -N;  // Можно убрать, добавив модуль в следующее условие
+if (99 < N && N < 1000)
 {
-    int LastDigit = N % 10; // Последняя цифра это остаток от деления на 10
-    Console.Write(LastDigit);
+        int LastDigit = N % 10; // Последняя цифра это остаток от деления на 10
+        Console.Write(LastDigit);
 }
 else
 {

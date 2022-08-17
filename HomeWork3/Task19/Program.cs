5e177a6f2a3a29ae93a3c8129168335c80aaf671
Daria <iwahnowa.darja@yandex.ru>
2022-08-17 21:20:24 +0300

    finished task19

--- HomeWork3/Task19/Program.cs
+++ HomeWork3/Task19/Program.cs
@@ -6,25 +6,26 @@
 
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
-int number = Console.ReadLine();
-if (number < 10000 || 99999 < number)
+int number = Convert.ToInt32(Console.ReadLine());
+int firstTwoDigits = Convert.ToInt32(number / 1000);
+int fourthDigit = Convert.ToInt32((number / 10) % 10);
+int fifthDigit = Convert.ToInt32(number % 10);
+if (9999 < number && number < 100000)
 {
-	Console.Write("Число введено некорректно"); 
-	break; 
-	Console.Write(dotnet run);
-}
-
-int firstDigit = Convert.ToInt32(number/1000) //рассматривать сразу двухзначное число
-int secondDigit = Convert.ToInt32(number/100) //
-int fourthDigit = Convert.ToInt32(number%100)
-int fifthDigit = Convert.ToInt32(number%10)
-
-int temp = fourthDigit;
-fourthDigit = fifthDigit;
-fifthDigit = temp;
-
-if (firstDigit*10+secondDigit == fourthDigit*10+fifthDigit);
+    int temp = fourthDigit; // обмен значениями переменных (4-й и 5-й цифры)
+    fourthDigit = fifthDigit;
+    fifthDigit = temp;
+    
+    if (firstTwoDigits == fourthDigit * 10 + fifthDigit)
+    {
+        Console.Write($"{number} является палиндромом");
+    }
+    else
+    {    
+        Console.Write($"{number} не является палиндромом");
+    }
+}    
+else
 {
-	Console.Write($"{number} является палиндромом");
+    Console.Write("Введено некорректное число");
 }
-else Console.Write($"{number} не является палиндромом");

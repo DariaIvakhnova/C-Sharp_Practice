867647f3321b147d4e72dfc95f1f33254c5fe746
Daria <iwahnowa.darja@yandex.ru>
2022-08-22 03:14:39 +0300

    finish HW4

--- HomeWork4/Task27/Program.cs
+++ HomeWork4/Task27/Program.cs
@@ -4,3 +4,10 @@
 Console.Clear();
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+int sumDigits = 0;
+while (number > 0)
+{
+    sumDigits = sumDigits + (number%10);
+    number = number / 10;
+}
+Console.Write($"Сумма цифр равна {sumDigits}");

05b0a3b2c1afc339c379b8f754245b89d7281533
DariaIvakhnova <109278319+DariaIvakhnova@users.noreply.github.com>
2022-08-29 23:59:25 +0300

    update task52

--- HomeWork7/Task_52/Program.cs
+++ HomeWork7/Task_52/Program.cs
@@ -64,3 +64,46 @@ double = ColumnAverage(int[,] matrix)
      }
      return columnAver = columnSum/matrix.GetLength(1);
 }
+
+// вариант от Гаи:
+void NewRandomMatrix (int[,] matrix)
+{
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(10, 100);
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Сначала объединила эти два метода, но потом передумала и вернула как было
+
+void FindTheElement (int str, int col, int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        if (str - 1 >= matr.GetLength(0) || col - 1 >= matr.GetLength(1))
+        {
+            System.Console.WriteLine("There is no such position in this matrix");
+            break;
+        }    
+    
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == str - 1 && j == col - 1)
+            {
+                System.Console.WriteLine($"The 
+System.Console.WriteLine("What string is the element on? : ");
+int str = int.Parse(Console.ReadLine() ?? "0");
+
+System.Console.WriteLine("Which column is the element in? : ");
+int col = int.Parse(Console.ReadLine() ?? "0");
+System.Console.WriteLine();
+
+int[,] matrix = new int[6, 6];
+NewRandomMatrix(matrix);
+FindTheElement(str, col, matrix);

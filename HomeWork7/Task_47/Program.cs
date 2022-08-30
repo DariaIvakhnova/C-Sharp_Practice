5442aeb7a917530032a49820669ead7fe2d55702
Daria <iwahnowa.darja@yandex.ru>
2022-08-31 01:56:47 +0300

    fill hw7

--- HomeWork7/Task_47/Program.cs
+++ HomeWork7/Task_47/Program.cs
@@ -4,3 +4,29 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+double [,] NewCustomMatrix (int numLines, int numColumns, double minRand, double maxRand)
+{
+  double[,] matrix = new double[numLines, numColumns];
+  Random rand = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = rand.NextDouble() * (maxRand - minRand) + minRand; // формула для увеличения диапазона рандома вещественных чисел; 
+      Console.Write($"{matrix[i, j]:F1}\t");
+    }
+    Console.WriteLine();
+  }
+  return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int numLines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int numColumns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальную границу диапазона значений: ");
+double minRand = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите максимальную границу диапазона значений: ");
+double maxRand = Convert.ToDouble(Console.ReadLine());
+
+double [,] newMatrix = NewCustomMatrix (numLines, numColumns, minRand, maxRand);
--- HomeWork7/Task_50/Program.cs
+++ HomeWork7/Task_50/Program.cs
@@ -6,3 +6,52 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+int[,] NewRandomMatrix(int lines, int columns, int minRand, int maxRand)
+{
+    int[,] matrix = new int[lines, columns];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next();
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    return matrix;
+}
+
+
+
+int[,] matrix = new int[3, 4];
+Random rand = new Random();
+int counterOfLines = 0;
+int counterOfColumns = 0;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rand.Next(1, 9);
+        Console.Write($"{matrix[i, j]}\t");
+        counterOfColumns++;
+    }
+    Console.WriteLine();
+    counterOfLines++;
+}
+
+int[,] newMatrix = NewRandomMatrix(3, 4, 1, 10);
+
+Console.WriteLine("Введите индекс строки элемента: ");
+int numLine = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца элемента: ");
+int numColumn = Convert.ToInt32(Console.ReadLine());
+
+if (numLine <= counterOfLines && numColumn <= counterOfColumns)
+{
+    Console.WriteLine($"Элемент с индексами [{numLine},{numColumn}] : {matrix[numLine, numColumn]}");
+}
+else
+{
+    Console.WriteLine("Элемента с такими индексами не существует в данном массиве.");
+}

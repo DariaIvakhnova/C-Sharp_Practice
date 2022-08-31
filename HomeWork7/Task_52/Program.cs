afc176c56c898775f31e28477a0acebb6ead2b2a
Daria <iwahnowa.darja@yandex.ru>
2022-09-01 01:19:02 +0300

    finished all

--- HomeWork7/Task_52/Program.cs
+++ HomeWork7/Task_52/Program.cs
@@ -6,3 +6,47 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+ void FillMatrix(int[,] matr)
+ {
+    Random rand = new Random();
+     for (int i = 0; i < matr.GetLength(0); i++)
+     {
+         for (int j = 0; j < matr.GetLength(1); j++)
+         {
+             matr[i, j] = rand.Next(1, 10);
+         }
+     }
+}
+
+ void PrintMatrix(int[,] massif)
+ {
+     for (int i = 0; i < massif.GetLength(0); i++)
+     {
+         for (int j = 0; j < massif.GetLength(1); j++)
+         {
+             Console.Write($"{massif[i, j]}\t");
+         }
+         System.Console.WriteLine();
+     }
+}
+
+int GetAverageForColumn(int[,] matrix)
+{
+    int sumForColumn = 0;
+    double averageForColumn = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++) // Меняем местами очередность (i <-> j) прохождения по массиву,
+    {                                             // т.о. проходим поочередно столбцы и считаем сумму элементов в них.
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sumForColumn += matrix[i, j];
+        }
+        averageForColumn = (double) sumForColumn/matrix.GetLength(0);
+        Console.WriteLine($"Среднее арифметическое значений {j+1} столбца = {averageForColumn.ToString("F1")};");
+    }
+    return sumForColumn;
+}
+
+int[,] matrix = new int[3, 4];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+GetAverageForColumn(matrix); 

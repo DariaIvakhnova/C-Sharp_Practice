0eb9ab0272fd7d966859e7a38f535606fbf15c92
Daria <iwahnowa.darja@yandex.ru>
2022-09-22 00:18:57 +0300

    66 done

--- HomeWork9/Task_66/Program.cs
+++ HomeWork9/Task_66/Program.cs
@@ -3,3 +3,27 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int SumInsideInterval(int start, int end)
+{
+    if (start == end) 
+    {
+        return start;
+    }
+    return start + SumInsideInterval(start+1, end);
+}
+
+Console.Clear();
+Console.Write("Введите M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int numberN = int.Parse(Console.ReadLine());
+int temp = 0;
+if (numberM > numberN) // цикл обмена значениями для случая, когда М > N
+{
+    temp = numberM;
+    numberM = numberN;
+    numberN = temp;
+}
+
+SumInsideInterval(numberM,numberN);
+Console.WriteLine($"Сумма натуральных элементов в промежутке [M;N] = {(SumInsideInterval(numberM,numberN))}");

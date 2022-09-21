d1656673461c65cde21812d20a2766bc35e12de4
Daria <iwahnowa.darja@yandex.ru>
2022-09-22 02:50:32 +0300

    68 done

--- HomeWork9/Task_68/Program.cs
+++ HomeWork9/Task_68/Program.cs
@@ -2,3 +2,29 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 29
 
+
+int Accerman(int m, int n) // метод для вычисления функции Аккермана
+{
+    if (n == 0) 
+    {
+        return m + 1;
+    }    
+    if (n > 0 && m == 0) 
+    {
+        return Accerman(1, n - 1);
+    }
+    return Accerman(Accerman(m - 1, n), n - 1);
+}
+
+int numberM;
+int numberN;
+do {    // цикл для отсечения ввода отрицательных чисел
+    Console.Clear();
+    Console.WriteLine("Введите неотрицательные числа.");
+    Console.Write("Введите M: ");
+    Int32.TryParse((Console.ReadLine()!), out numberM);
+    Console.Write("Введите N: ");
+    Int32.TryParse((Console.ReadLine()!), out numberN);
+} while (numberM < 0 || numberN < 0);
+
+Console.WriteLine($"Результат вычисления функции Аккермана = {Accerman(numberM, numberN)}");
